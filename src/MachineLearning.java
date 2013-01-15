5b9675ee4133b1442d7df9ea1aa61772e243e837
Nevik Rehnel <hai.kataker@gmx.de>
2013-01-15 22:09:54 +0100

    Add simple multi-threading

--- src/MachineLearning.java
+++ src/MachineLearning.java
@@ -2,11 +2,15 @@ public class MachineLearning {
 
     private static int TOTAL_RUNS = 3000;
     private static int ARMS = 10;
+    private static int ROUNDS = 10000;
+    private static int THREADS = 2;
 
     public static void main(String[] args) {
         int i, goodRuns = 0;
+        WorkerThread threads[] = new WorkerThread[THREADS];
+        //for (int j = 0; j < THREADS; j++)
 
-        for (i = 1; i <= TOTAL_RUNS; ++i) {
+        /*for (i = 1; i <= TOTAL_RUNS; ++i) {
             NArmedBandit bandit = new NArmedBandit(ARMS);
             Player player = new SimplePlayer(bandit);
             int best = player.play(1000000);
@@ -16,7 +20,32 @@ public class MachineLearning {
                 printStats(i, goodRuns);
                 printProgress(i);
             }
+        }*/
+        
+        i = 1;
+        while (i <= TOTAL_RUNS) {
+            for (int j = 0; j < THREADS; j++)
+                if (threads[j] == null) {
+                    threads[j] = new WorkerThread(ARMS, ROUNDS);
+                    threads[j].start();
+                    i++;
+                }
+                else if (threads[j].isDone()) {
+                    if (threads[j].player.getBestArmIndex() == threads[j].bandit.getBestArmIndex())
+                        goodRuns++;
+                    threads[j] = new WorkerThread(ARMS, ROUNDS);
+                    threads[j].start();
+                    i++;
+                }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {}
+            if (i % 10 == 0) {
+                //printStats(i, goodRuns);
+                printProgress(i);
+            }
         }
+        
         printStats(i, goodRuns);
     }
 
@@ -34,5 +63,30 @@ public class MachineLearning {
         System.out
                 .print("\tprogress: " + String.format("%.1f", progress) + "%");
         System.out.println();
+    }
+    
+    private static class WorkerThread extends Thread {
+        private boolean done = false;
+        public NArmedBandit bandit;
+        public Player player;
+        private int rounds;
+
+        public WorkerThread(int arms, int rounds) {
+            super();
+            this.bandit = new NArmedBandit(arms);
+            this.player = new SimplePlayer(bandit);
+            this.rounds = rounds;
+        }
+
+        @Override
+        public void run() {
+            if (!done) {
+                player.play(rounds);
+                done = true;
+            }
+        }
+        
+        public boolean isDone(){return done;}
+    
     }
 }
